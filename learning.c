57b0132295647f832a1fa3eea9852dbb3fc61b16
Yash <yash20004x17@gmail.com>
2024-05-30 17:08:49 +0530

    Added a C code file

---
+++ learning.c
@@ -0,0 +1,94 @@
+// #include<stdio.h>
+// int main(){
+//     float radius;
+//     printf("Enter radius of circle :");
+//     scanf("%f",&radius);
+//     float area = 3.1415926535 * radius * radius;
+//     printf("Area of circle with radius %f is %f",radius,area);
+//     return 0;
+// }  This code is to calculate the area of circle.
+
+// #include <stdio.h>
+// int main()
+// {
+
+//     float length, width, perimeter;
+//     printf("Enter length of rectangle :");
+//     scanf("%f", &length);
+//     printf("Enter width of rectangle :");
+//     scanf("%f", &width);
+//     perimeter = 2 * (length + width);
+//     printf("Perimeter of rectangle with %f and %f is %f", length, width, perimeter);
+
+//     return 0;
+// } This code is to calculate the perimeter of rectangle.
+
+// #include <stdio.h>
+// int main()
+// {
+
+//     float number, cube;
+//     printf("Enter number :");
+//     scanf("%f", &number);
+
+//     cube = number * number * number;
+//     printf("Cube of number %f is %f", number, cube);
+
+//     return 0;
+// } This code is to calculate the cube of a number.
+
+//  #include <stdio.h>
+//  #include <math.h>
+
+//  int main(){
+//     int b = 2 , c = 2;
+//     int power = pow(b,c);
+//     printf("%d",power);
+//     return 0;
+//  } In this code we have used a new library math.h it contains
+//  all the functions of maths and a new function "pow" used to represent 2^4 and like 3^2.
+
+// #include <stdio.h>
+
+// int main() {
+
+//  char ch;
+
+//  printf("Enter a character: ");
+
+//  scanf("c", &ch);
+
+//  printf("ASCII value of %c = %d\n", ch, ch);
+
+//  return 0;
+
+// }
+// #include <stdio.h>
+// int main()
+// {
+//     float ans, a = 1.6, b = 1;
+//     ans = a % b;
+
+//     printf("%f", ans);
+//     return 0;
+// }
+// //This code shows that % modulo operator does not work on float values and can be only be used with integer values.
+// #include <stdio.h>
+// int main()
+// {
+
+//     printf("%d", -5%2);
+//     return 0;
+// // } whenever our numerator is negative, our remainder is also negative
+
+
+// #include <stdio.h>
+// int main()
+// {
+//     int a= 4 , b = 5;
+//     printf("%d\n", a !=b);
+//         printf("%d\n", a ==b);
+      
+//     return 0;
+// }
+
