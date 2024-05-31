66ec09570fce1e2afb843f628a2f68cdd8153cd5
Yash <yash20004x17@gmail.com>
2024-05-31 19:55:31 +0530

    Code update

--- practice.c
+++ practice.c
@@ -1,14 +1,20 @@
+// #include <stdio.h>
+// int main()
+// {
+//     int cash1, cash2, total;
+//     printf("Enter the amount you have :");
+//     scanf("%d", &cash1);
+//     printf("Enter the amount you have :");
+//     scanf("%d", &cash2);
+
+//     total = cash1 + cash2;
+//     printf("Total cash available is %d", total);
+
+//     return 0;
+// }
+
 #include <stdio.h>
-int main()
-{
-    int cash1, cash2, total;
-    printf("Enter the amount you have :");
-    scanf("%d", &cash1);
-    printf("Enter the amount you have :");
-    scanf("%d", &cash2);
-
-    total = cash1 + cash2;
-    printf("Total cash available is %d", total);
-
+int main(){
+    printf("%d",5+2/2*3);
     return 0;
 }
