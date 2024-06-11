48a8c44a7788f0d47ff6d6d1493360090a807d3c
Yash <yash20004x17@gmail.com>
2024-06-11 21:07:43 +0530

    Continuing loops in C

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,48 +1,47 @@
-#include <stdio.h>
-int main()
-{
-   // for(int i = 1; i <= 9  ; i++){
-   //    printf("%d\n",i) ;
-   // }
-   // return 0;
-   // int i=1;
-   // while (i<=9)
-   // {
-   //   printf("%d\n",i) ;
-   //   i++;
-   // }
+// #include <stdio.h>
+// int main()
+// {
+//    // for(int i = 1; i <= 9  ; i++){
+//    //    printf("%d\n",i) ;
+//    // }
+//    // return 0;
+//    // int i=1;
+//    // while (i<=9)
+//    // {
+//    //   printf("%d\n",i) ;
+//    //   i++;
+//    // }
 
-   // int i= 1;
-   // do
-   // {
-   // printf("%d\n",i) ;
-   // i++;
-   // } while (i<=9);
+//    // int i= 1;
+//    // do
+//    // {
+//    // printf("%d\n",i) ;
+//    // i++;
+//    // } while (i<=9);
+// //    int sum = 0, n;
+// //    printf("Enter Natural number :");
+// //    scanf("%d", &n);
 
-//    int sum = 0, n;
-//    printf("Enter Natural number :");
-//    scanf("%d", &n);
-
-//    for (int i = 1; i <= n; i++)
-//    {
-//       sum = sum + i;
+// //    for (int i = 1; i <= n; i++)
+// //    {
+// //       sum = sum + i;
      
-//    }
-//    printf("%d\n", sum);
+// //    }
+// //    printf("%d\n", sum);
 
-// for(int i=n; i>=1; i--){
-//    printf("%d\n",i);
-//    }
+// // for(int i=n; i>=1; i--){
+// //    printf("%d\n",i);
+// //    }
 
-int n, sum=0;
-printf("Enter a natural number :");
-scanf("%d",&n);
+// int n, sum=0;
+// printf("Enter a natural number :");
+// scanf("%d",&n);
 
 
-for( int i=1; i>=n; i++){
-   sum = sum + i;
-   printf("%d\n",sum);
-}
+// for( int i=1; i>=n; i++){
+//    sum = sum + i;
+//    printf("%d\n",sum);
+// }
 
-   return 0;
-}
+//    return 0;
+// }
