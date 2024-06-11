e5696a52aca0642e709afc7912ee76ee074712b9
Yash <yash20004x17@gmail.com>
2024-06-11 20:31:06 +0530

    Continuing loops in C

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,23 @@
 #include<stdio.h>
 int main(){
-   char ch;
-   printf("Enter charachter :");
-   scanf("%c",&ch);
+   // for(int i = 1; i <= 9  ; i++){
+   //    printf("%d\n",i) ;
+   // }
+   // return 0;
+   // int i=1;
+   // while (i<=9)
+   // {
+   //   printf("%d\n",i) ;
+   //   i++;
+   // }
+
+
+   // int i= 1;
+   // do
+   // {
+   // printf("%d\n",i) ;
+   // i++;
+   // } while (i<=9);
+   
+   
+}
