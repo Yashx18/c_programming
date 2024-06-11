601ad21cdb6a54332e91bf3a0fe6631341e3c502
Yash <yash20004x17@gmail.com>
2024-06-11 21:05:47 +0530

    Continuing loops in C

--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,5 +1,6 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+int main()
+{
    // for(int i = 1; i <= 9  ; i++){
    //    printf("%d\n",i) ;
    // }
@@ -11,13 +12,37 @@ int main(){
    //   i++;
    // }
 
-
    // int i= 1;
    // do
    // {
    // printf("%d\n",i) ;
    // i++;
    // } while (i<=9);
-   
-   
+
+//    int sum = 0, n;
+//    printf("Enter Natural number :");
+//    scanf("%d", &n);
+
+//    for (int i = 1; i <= n; i++)
+//    {
+//       sum = sum + i;
+     
+//    }
+//    printf("%d\n", sum);
+
+// for(int i=n; i>=1; i--){
+//    printf("%d\n",i);
+//    }
+
+int n, sum=0;
+printf("Enter a natural number :");
+scanf("%d",&n);
+
+
+for( int i=1; i>=n; i++){
+   sum = sum + i;
+   printf("%d\n",sum);
+}
+
+   return 0;
 }
